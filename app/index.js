15fc275cc2859303ddb5fa5efd649ac4f888beb6
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-04-27 13:55:45 -0400

    updated carousel

--- app/index.js
+++ app/index.js
@@ -1,21 +1,18 @@
 const carousel = document.getElementsByClassName("carousel")[0]
-const carouselButtons = document.getElementsByClassName("carousel-buttons")[0].children
+const carouselButtons = document.getElementsByClassName("carousel-button-container")[0].children
 
 setInterval(() => {
-    if (carousel.scrollLeft !== 700) {
-        carousel.scrollBy({ left: 1, top: 0, behavior: "smooth" })
+    if (carousel.scrollLeft !== 1800) {
+        carousel.scrollBy({ left: 900, top: 0, behavior: "smooth" })
     } else {
-        carousel.scrollBy({ left: -800, top: 0, behavior: "smooth" })
+        carousel.scrollBy({ left: -1800, top: 0, behavior: "smooth" })
     }
 }, 4000)
 
-console.log(carouselButtons)
-
 for (let i = 0; i < carouselButtons.length; i++) {
     carouselButtons[i].addEventListener("click", () => {
-        console.log("test")
         carousel.scrollTo({
-            left: (i * 350), top: 0, behavior: "smooth"
+            left: (i * 900), top: 0, behavior: "smooth"
         })
     })
 }
