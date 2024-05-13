e3ffe0943af3db2713226e805029853a54c3be1d
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-05-13 17:28:17 -0400

    fix

--- stories/story-layout-test/test.js
+++ stories/story-layout-test/test.js
@@ -1,5 +1,5 @@
-import cam from "/shared_files/classes/webcam.js";
-import EmotionProfiles from "/shared_files/classes/emotionProfile.js";
+import cam from "../../shared_files/classes/webcam";
+import EmotionProfiles from "../../shared_files/classes/emotionProfile";
 
 const reactionButton = document.getElementById("webcamButton");
 const childElement = document.querySelector('.story-scenery');
