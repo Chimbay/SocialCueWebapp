78517a10a2c79b7a4b7e0e46cce0e2654777b6b1
Chimbay <c.anthony.priv@gmail.com>
2024-06-01 18:50:23 -0400

    Made additions to migration

--- react-migration/src/App.jsx
+++ react-migration/src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
 import Navbar from './components/Navbar'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import TypesOfEmotions from './pages/TypesOfEmotions';
 
 function App() {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/TypesOfEmotions" element={<TypesOfEmotions />} />
+            </Routes>
+        </>
     )
 }
 
