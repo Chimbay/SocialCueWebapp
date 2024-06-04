39eb17d6e04267b240f88e1a2f6ce49f790fbde5
Chimbay <c.anthony.priv@gmail.com>
2024-06-04 04:04:57 -0400

    Added stories page

--- react-migration/src/App.jsx
+++ react-migration/src/App.jsx
@@ -1,8 +1,10 @@
 import Navbar from './components/Navbar'
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
 import TypesOfEmotions from './pages/TypesOfEmotions';
+import Stories from './pages/Stories';
 
 function App() {
     return (
@@ -11,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/TypesOfEmotions" element={<TypesOfEmotions />} />
+                <Route path="/Stories" element={<Stories />} />
             </Routes>
         </>
     )
