a6f6f598de19ec5348bb176a8d4deb9cf1c765fc
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-06-08 11:30:55 -0400

    converted type of emotion page to react

--- react-migration/src/pages/TypesOfEmotions/index.jsx
+++ react-migration/src/pages/TypesOfEmotions/index.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./index.css";
+import close from "../../images/close.png";
 
+const emotions = ["Happy", "Sad", "Angry", "Surprised", "Scared", "Anxious"]
 const emotionDescriptions = {
     Happy: "Imagine feeling like you're a shiny balloon, floating high in the sky with a big, warm smile on your face. Your heart feels as light as a feather, and there's a sparkle in your eyes like stars in the night sky. You might feel like jumping for joy or dancing around because everything feels so wonderful and bright!",
     Sad: "Picture a rainy day when the clouds cover the sky, and you feel like a droopy flower. Your heart feels heavy, like a rock sinking in a pond. Tears might fill your eyes, and you might want to hug your favorite stuffed animal tight for comfort. It's okay to feel sad sometimes, just like the rain, it will pass.",
@@ -10,41 +12,43 @@ const emotionDescriptions = {
     Anxious: "Anxiety feels like a little bug buzzing around in your tummy, making you feel jittery and restless. Your mind might race like a speedy car, and you might find it hard to sit still. It's important to take deep breaths and remember that everything will be okay, even if it feels a bit overwhelming right now."
 }
 
-// for (const element of emotion) {
-//     element.addEventListener("click", () => {
-//         modal.showModal()
-//         modalEmotionHeading.innerText = element.innerText
-//         modalEmotionDescription.innerText = emotionDescriptions[element.innerText]
-//     })
-// }
+function EmotionTypeGridBox({ emotion, modalRef, changeModalText }) {
+    function openModal() {
+        modalRef.current.showModal()
+        changeModalText({
+            h: emotion,
+            p: emotionDescriptions[emotion]
+        })
+    }
+
+    return (
+        <div className="emotion-wrapper" onClick={openModal}>
+            <div className="emotion">{emotion}</div>
+        </div >
+    )
+}
 
 export default function TypesOfEmotions() {
+    const [dialogContent, setDialogContent] = useState({ h: "", p: "" })
+    const dialogRef = useRef(null)
+
+    function closeModal() {
+        dialogRef.current.close()
+        setDialogContent({ h: "", p: "" })
+    }
+
     return (
         <>
-            <div className="container" id="container">
-                <div className="emotion-wrapper">
-                    <div className="emotion">Happy</div>
-                </div>
-                <div className="emotion-wrapper">
-                    <div className="emotion">Sad</div>
-                </div>
-                <div className="emotion-wrapper">
-                    <div className="emotion">Angry</div>
-                </div>
-                <div className="emotion-wrapper">
-                    <div className="emotion">Surprised</div>
-                </div>
-                <div className="emotion-wrapper">
-                    <div className="emotion">Scared</div>
-                </div>
-                <div className="emotion-wrapper">
-                    <div className="emotion">Anxious</div>
-                </div>
+            <div className="container">
+                {emotions.map((emote, index) => <EmotionTypeGridBox
+                    emotion={emote}
+                    modalRef={dialogRef}
+                    changeModalText={setDialogContent} key={`grid-box-${emote}-${index}`} />)}
             </div>
-            <dialog id="emotion-modal">
-                <h1 id="emotion-heading"></h1>
-                <button id="exit">x</button>
-                <p id="emotion-description"></p>
+            <dialog className="emotion-modal" ref={dialogRef}>
+                <h1 className="emotion-heading">{dialogContent.h}</h1>
+                <img className="exit" src={close} alt="close-button" onClick={closeModal} />
+                <p className="emotion-description">{dialogContent.p}</p>
             </dialog>
         </>
     )
