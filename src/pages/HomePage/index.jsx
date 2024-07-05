827802b193f567385f7ea05459e13e74de785734
Anthony <c.anthony.priv@gmail.com>
2024-07-05 15:41:58 -0400

    Changes to homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -8,20 +8,38 @@ import HomeCarousel from "../../components/HomeCarousel";
 export default function HomePage() {
   const [currentEmotion, setCurrentEmotion] = useState(null);
 
+  const progressBar = {
+
+  }
+  const progressModes = {
+    mode: currentEmotion,
+    containers: [
+      { completion: 10, background: "ek" },
+      { completion: 10, background: "ek" },
+      { completion: 10, background: "ek" },
+    ],
+  };
+
   return (
     <div className={style.mainContent}>
       <div className={style.bodyContent}>
-        <div className={style.progressSection}>
+        <div className={style.progressBar}>
           <HomeProgress />
         </div>
-        <div className={style.slideCarousel}>
-          <HomeCarousel/>
-        </div>
+        <div className={style.centerContent}>
+          <div className={style.progressModes}>
+            {progressModes.containers.map((container, index) => {
 
+            })}
+            <div className={style.slideCarousel}>
+              <HomeCarousel />
+            </div>
+          </div>
+        </div>
       </div>
       <footer className={style.footerContainer}>
         <div className={style.bottomNav}></div>
-        <div className={style.carouselHolder}>
+        <div className={style.roundCarousel}>
           <HomeRoundCarousel />
         </div>
       </footer>
