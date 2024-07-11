99f97a5c2ab64a06839722865352f05b50deff87
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2024-07-11 12:00:16 -0400

    updated to closer match figma

--- src/pages/AccountForm/index.jsx
+++ src/pages/AccountForm/index.jsx
@@ -14,7 +14,7 @@ export default function AccountForm() {
 
     if (formStatus == "signup") {
       console.log(
-        `Username: ${username}\nPassword: ${password}\nConfirm Password: ${confirmPassword}`,
+        `Username: ${username}\nPassword: ${password}\nConfirm Password: ${confirmPassword}`
       );
     } else if (formStatus == "login") {
       console.log(`Username: ${username}\nPassword: ${password}\n`);
@@ -23,18 +23,41 @@ export default function AccountForm() {
 
   return (
     <form onSubmit={handleSubmit} className={style.form}>
-      <h1>{formStatus == "signup"}</h1>
-      <input type="text" placeholder="Username" className={style.input} />
-      <input type="password" placeholder="Password" className={style.input} />
+      <h1>
+        {formStatus == "signup"
+          ? "Sign up to make your account!"
+          : "Log in to your account!"}
+      </h1>
       {formStatus == "signup" && (
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          className={style.input}
-        />
+        <>
+          <div className={style.typeButtonContainer}>
+            <a className={style.accountType}>Parents/Gaurdians</a>
+            <a className={style.accountType}>Kids</a>
+          </div>
+          <label className={style.label}>
+            First Name
+            <input type="password" className={style.input} />
+          </label>
+          <label className={style.label}>
+            Last Name
+            <input type="password" className={style.input} />
+          </label>
+        </>
       )}
+      <label className={style.label}>
+        Email Address
+        <input type="text" className={style.input} />
+      </label>
+      <label className={style.label}>
+        Your Password
+        <input type="password" className={style.input} />
+      </label>
+      <label className={style.label}>
+        Confirm Your Password
+        <input type="password" className={style.input} />
+      </label>
       <button type="submit" className={style.submitButton}>
-        Login!
+        {formStatus == "signup" ? "Sign Up" : "Log In"}
       </button>
     </form>
   );
