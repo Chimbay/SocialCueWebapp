8153b22c315377b885ea1b7f7f82c1a5dbc332b4
Anthony <c.anthony.priv@gmail.com>
2024-07-16 18:06:29 -0400

    Pushes

--- src/components/EmotionQuiz/index.jsx
+++ src/components/EmotionQuiz/index.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import style from "./index.module.css";
-import happpyImage from "../../images/children-playing-tag.jpg";
+import happyImage from "../../images/readingFamily.png";
+import icecreamFamily from "../../images/icecreamFamily.png";
+import grandmaFamily from "../../images/grandmaFamily.png";
+
 import { useParams } from "react-router-dom";
 const explore = [
   {
@@ -18,14 +21,22 @@ const explore = [
       quiz: {
         questions: [
           {
-            question: "Are they happy",
-            Answer: "Yes",
+            question:
+              "Today my mom took me to the playground and bought me my favorite ice cream. I felt very happy",
+            answer: true,
+            src: happyImage,
           },
           {
-            question: "Are they happy",
+            question:
+              "Today my brother read two of my favorite books to me. I love my brother, and I felt very happy",
+            answer: true,
+            src: icecreamFamily,
           },
           {
-            question: "Are they happy",
+            question:
+              "Today I visited my grandparents and swam in the lake with them. I felt very happy",
+            answer: true,
+            src: grandmaFamily,
           },
           {
             question: "Are they happy",
@@ -40,9 +51,12 @@ export default function EmotionQuiz() {
   const { groundPathID } = useParams();
 
   const [quiz, setQuiz] = useState(null);
-  const [questions, setQuestions] = useState(null)
+  const [questions, setQuestions] = useState(null);
   const [currentQuestion, setCurrentQuestion] = useState(null);
   const [currentQuestionNumber, setCurrentQuestionNumber] = useState(null);
+  const [visible, setVisible] = useState(false);
+  const [answer, setAnswer] = useState(null);
+  const timerRef = useRef(null);
 
   useEffect(() => {
     const emotionFetch = explore.find((dir) => dir.category === groundPathID);
@@ -52,11 +66,11 @@ export default function EmotionQuiz() {
       const questions = quiz.questions;
       setQuiz(quiz);
       setQuestions(questions);
-      setCurrentQuestion(questions[0])
-      setCurrentQuestionNumber(0)
+      setCurrentQuestion(questions[0]);
+      setCurrentQuestionNumber(0);
     }
   }, []);
-  
+
   useEffect(() => {
     if (quiz) {
       const updatedQuiz = questions[currentQuestionNumber];
@@ -64,50 +78,75 @@ export default function EmotionQuiz() {
     }
   }, [currentQuestionNumber]);
 
-  function checkRightQuestionBound() {
-    return currentQuestionNumber < quiz.length - 1;
-  }
   function checkLeftQuestionBound() {
     return currentQuestionNumber > 0;
   }
+  function checkRightQuestionBound() {
+    return currentQuestionNumber < questions.length - 1;
+  }
   function previousQuestion() {
     if (checkLeftQuestionBound())
-      setCurrentPageNumber((number) => (number = number - 1));
+      setCurrentQuestionNumber((number) => (number = number - 1));
   }
   function nextQuestion() {
     if (checkRightQuestionBound())
-      setCurrentPageNumber((number) => (number = number + 1));
+      setCurrentQuestionNumber((number) => (number = number + 1));
   }
 
-  function isCorrectAnswer(answer){
-    
-  }
+  const isCorrectAnswer = (isCorrect) => {
+    setAnswer(isCorrect ? "correct" : "incorrect");
+    startTimer();
+  };
+  const startTimer = () => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    timerRef.current = setTimeout(() => {
+      setVisible(false);
+      setTimeout(() => {
+        setAnswer(null);
+      }, 500); // Match the transition duration
+    }, 2000);
+  };
 
   return (
     <div className={style.mainContent}>
       {currentQuestion ? (
-        <>
-          <div className={style.quizContainer}>
-            <div className={style.quizTitle}>
-                {currentQuestion.question}
-            </div>
-            <div className={style.quizContent}>
-              <img className={style.quizImage} src={happpyImage}></img>
-            </div>
-            <div className={style.answerOptions}>
-              <button className={style.answerOption} onClick={isCorrectAnswer}>0</button>
-              <button className={style.answerOption} onClick={isCorrectAnswer}>1</button>
-            </div>
-            <div className={style.quizNavigation}>
-              <button className={style.previousQuestion} onClick={previousQuestion}>
-                prev
-              </button>
-              <button className={style.nextQuestion} onClick={nextQuestion}>
-                next
-              </button>
-            </div>
+        <div className={style.quizContainer}>
+          <div className={style.quizTitle}>{currentQuestion.question}</div>
+          <div className={style.quizContent}>
+            <img className={style.quizImage} src={currentQuestion.src}></img>
           </div>
-        </>
+          <div className={style.answerOptions}>
+            <button
+              className={style.answerOption}
+              onClick={() => isCorrectAnswer(false)}
+            >
+              No.
+            </button>
+            <button
+              className={style.answerOption}
+              onClick={() => isCorrectAnswer(true)}
+            >
+              Yea!
+            </button>
+          </div>
+          <div className={style.quizNavigation}>
+            <button
+              className={style.previousQuestion}
+              onClick={previousQuestion}
+            >
+              prev
+            </button>
+            <button className={style.nextQuestion} onClick={nextQuestion}>
+              next
+            </button>
+          </div>
+          <div className={`answer ${visible ? "show" : ""}`}>
+            {answer === "correct" && "Correct"}
+            {answer === "incorrect" && "Incorrect"}
+          </div>
+        </div>
       ) : null}
     </div>
   );
